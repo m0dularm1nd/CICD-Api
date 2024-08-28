d4cdf10284545204761c1de575f71bfb9f6cfbcb
m0dularm1nd <fedora@home.xyz>
2024-08-29 01:26:59 +0300

    0-toching:0.1

--- src/app.js
+++ src/app.js
@@ -4,24 +4,26 @@ const app = express();
 
 app.get('/', (_req, res) => {
   res.status(200).json({
-    email: 'Hello from the other side!',
-    password: 'for manar',
+    message: 'Hello from the other side 👽',
+    user: 'Manar 👑',
+    email: 'hena@manar.com ☯️',
+    mshpassword: '☠️',
   });
 });
 
 app.get('/manar', (_req, res) => {
   res.status(200).json({
-    email: 'test@manar.com',
+    email: 'hena@manar.com',
     mshpassword: 'herewego',
   });
 });
 
 app.post('/', (_req, res) => {
-  res.send('Checking check... nothing...');
+  res.send('Checking check...');
 });
 
 app.post('/mohamed', (_req, res) => {
-  res.send('Checking check... herewego...');
+  res.send('Checking check... hmmmmmmmmm...');
 });
 
 const port = 3000;
