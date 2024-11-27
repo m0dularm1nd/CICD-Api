7c83c8111657e9626c26fda50d356d29ffed3206
m0dularm1nd <redhat@home.xyz>
2024-11-27 15:34:13 +0200

    multi-cd

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use((_req, res, next) => {
 
 app.get('/', (_req, res) => {
   res.status(200).json({
-    message: 'letsgoo',
+    message: 'Multi CD',
     user: 'Express.js Backend server',
     request: 'json response',
     email: 'here@V3il.com',
