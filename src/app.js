e84a3d68ef9da41cb9b518940f52f2ef9810ba10
m0dularm1nd <redhat@home.xyz>
2024-11-29 17:25:45 +0200

    2

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use((_req, res, next) => {
 
 app.get('/', (_req, res) => {
   res.status(200).json({
-    message: 'Multi CD, Multibranch',
+    message: 'Multi CD, Multibranch, vso',
     user: 'Express.js Backend server',
     request: 'json response',
     email: 'here@V3il.com',
