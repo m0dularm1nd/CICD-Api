e3b18ce7fcd8714247182d2ef03e96a4d331e4bd
m0dularm1nd <redhat@home.xyz>
2024-12-03 00:36:47 +0200

    1.3

---
+++ src/app.js
@@ -0,0 +1,48 @@
+import express from 'express';
+
+const app = express();
+
+app.use((_req, res, next) => {
+  let send = res.send;
+  res.send = c => {
+    console.log(`Code: ${res.statusCode}`);
+    console.log("Body: ", c);
+    res.send = send;
+    return res.send(c);
+  }
+  next();
+});
+
+app.get('/', (_req, res) => {
+  res.status(200).json({
+    message: 'Private Image, Multi CD, Multibranch, VSO',
+    user: 'Express.js Backend server',
+    request: 'json response',
+    email: 'here@V3il.com',
+    password: 'notpassword',
+    customImage: "nexus",
+    color: "sky",
+    shape: "pyramid",
+    image: "🔮"
+  });
+});
+
+app.get('/anotherendpoint', (_req, res) => {
+  res.status(200).json({
+    email: 'here@v3il.xyz',
+    token: 'test',
+  });
+});
+
+app.post('/', (_req, res) => {
+  res.send('yes...');
+});
+
+app.post('/postcheck', (_req, res) => {
+  res.send('🔮 Checking check...');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Up and Runing...\n |︻デ═一----------------------------------`);
+});
