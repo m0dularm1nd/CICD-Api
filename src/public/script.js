6309d1fdba04802aadd0729d5d4a28f291f74901
m0dularm1nd <moham3d9amal@gmail.com>
2025-01-26 20:46:15 +0200

    feat: newcolors

--- src/public/script.js
+++ src/public/script.js
@@ -48,11 +48,11 @@ async function loadMessages() {
         (msg) => `
     <div class="bg-gray-50 p-4 rounded-lg shadow-sm flex justify-between items-center">
         <div>
-            <span class="font-bold text-indigo-600">${msg.name}:</span>
+            <span class="font-bold text-violet-400">${msg.name}:</span>
             <span class="text-gray-700">${msg.message}</span>
         </div>
         <button onclick="deleteMessage(${msg.id})" 
-            class="text-sm bg-red-500 text-white px-2 py-2 rounded-md hover:bg-red-600 transition-colors">
+            class="text-sm bg-pink-300 text-white px-2 py-2 rounded-md hover:bg-pink-600 transition-colors">
             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
 	<path fill="currentColor" fill-rule="evenodd" d="M1.707.293A1 1 0 0 0 .293 1.707L5.586 7L.293 12.293a1 1 0 1 0 1.414 1.414L7 8.414l5.293 5.293a1 1 0 0 0 1.414-1.414L8.414 7l5.293-5.293A1 1 0 0 0 12.293.293L7 5.586z" clip-rule="evenodd" />
 </svg>
